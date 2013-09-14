e6ba0302b5a6086fd568cf0a19d0747c38504ce2
Antti Tonkyra <daedalus@pingtimeout.net>
2013-09-14 12:12:11 +0300

    initial dns_zone stuff

---
+++ server/modules/dns_zone/__init__.py
@@ -0,0 +1,2 @@
+from .routes import *
+from .database import *
---
+++ server/modules/dns_zone/database.py
@@ -0,0 +1,22 @@
+# encoding: utf-8
+
+"""
+Database objects for dns_zone
+"""
+
+from lib.database.table import RenkiBase, RenkiTable
+from sqlalchemy import Column,String,Boolean,Integer,ForeignKey
+from lib.database.tables import register_table
+
+class DNSZoneDatabase(RenkiBase, RenkiTable):
+	__tablename__ = 'dns_zone'
+	domain 		= Column('domain_id', 	Integer, 	ForeignKey('domain.id'))
+	refresh 	= Column('refresh', 	Integer, 	nullable=False)
+	retry 		= Column('retry', 		Integer, 	nullable=False)
+	expire 		= Column('expire', 		Integer, 	nullable=False)
+	ttl			= Column('ttl',			Integer, 	nullable=False)
+	rname		= Column('rname',		String, 	nullable=False)
+	record_ttl	= Column('record_ttl',	Integer,	nullable=False)
+
+# Register table
+register_table(DNSZoneDatabase)
