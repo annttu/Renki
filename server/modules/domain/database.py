aa7bbd84eb11e579d91c93207d81365fcec58a5c
Antti Jaakkola <annttu@git.annttu.fi>
2013-09-14 12:09:24 +0300

    Fixed some errors

--- server/modules/domain/database.py
+++ server/modules/domain/database.py
@@ -6,19 +6,20 @@ Domain objects database module
 
 from lib.database.table import RenkiUserTable, RenkiBase
 from lib.database.tables import register_table
-from lib.validators import validate_userid, validate_domain, validate_boolean
-from sqlalchemy import Column, String, Boolean
+from lib.validators import validate_userid, validate_domain
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class DomainDatabase(RenkiBase, RenkiUserTable):
     __tablename__ = 'domain'
     name = Column('name', String(1024), nullable=False)
-    dns_service = Column('dns_service', Boolean, nullable=False, default=True)
+    dns_zone_id = relationship("dns_zone", uselist=False, backref="domain")
+
 
     def validate(self):
         validate_userid(self.userid)
         validate_domain(self.name)
-        validate_boolean(self.dns_service, name='DNS_service')
 
     def save(self):
         super(DomainDatabase, self).save()
